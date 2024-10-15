05816639eb93f0639ba87582f9161a99004301b0
Vinicius Guedes <viniciusguedes.dev@gmail.com>
2024-10-14 21:04:10 -0300

    feat: new functionm to sum

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 export function multiply(a, b) {
     return a * b;
 }
+
+export function sum(a, b){
+    return a + b;
+}
--- test.js
+++ test.js
@@ -1,3 +1,4 @@
-import { multiply } from "./index.js";
+import { multiply, sum } from "./index.js";
 
-console.log(multiply(5, 10))
+console.log('multiply', multiply(5, 10))
+console.log('sum', sum(10, 10))
